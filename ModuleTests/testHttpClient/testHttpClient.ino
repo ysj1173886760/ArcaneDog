04da23b386209561e28c779a8ce1d2b6d843df08
ysj1173886760 <1173886760@qq.com>
2024-06-23 20:32:02 +0800

    support chat with glm

--- ModuleTests/testHttpClient/testHttpClient.ino
+++ ModuleTests/testHttpClient/testHttpClient.ino
@@ -1,7 +1,41 @@
 #include <WiFi.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
 
 const char* ssid = "";
 const char* password = "";
+
+String apiKey = "";
+String apiUrl = "";
+
+String getGPTAnswer(String inputTest) {
+  HTTPClient client;
+  client.setTimeout(10000);
+  client.begin(apiUrl);
+  client.addHeader("Content-Type", "application/json");
+  client.addHeader("Authorization", String(apiKey));
+
+  String messages = "\"messages\": [{\"role\": \"user\", \"content\": \"" + inputTest + "\"}]";
+  String model = "\"model\": \"glm-4\"";
+  String payload = "{" + model + "," + messages + "}";
+  Serial.println(payload);
+
+  int httpResponseCode = client.POST(payload);
+  if (httpResponseCode == 200) {
+    String response = client.getString();
+    client.end();
+    Serial.println(response);
+
+    DynamicJsonDocument jsonDoc(1024);
+    deserializeJson(jsonDoc, response);
+    String outputText = jsonDoc["choices"][0]["message"]["content"];
+    return outputText;
+  } else {
+    client.end();
+    Serial.printf("Error %i \n", httpResponseCode);
+    return "error";
+  }
+}
 
 void setup() {
   Serial.begin(115200);
@@ -27,6 +61,9 @@ void loop() {
   if (Serial.available()) {
     String inputText = Serial.readStringUntil('\n');
 
-    Serial.println("get: " + inputText);
+    Serial.println("read: " + inputText);
+
+    String answer = getGPTAnswer(inputText);
+    Serial.println("Answer: " + answer);
   }
 }
